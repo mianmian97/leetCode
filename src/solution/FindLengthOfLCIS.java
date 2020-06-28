adece052cfd1a1e146257d3ea0405d990745971d
zhangyibj01 <zhangyibj01@fenbi.com>
2020-06-28 22:01:29 +0800

    update

--- src/solution/FindLengthOfLCIS.java
+++ src/solution/FindLengthOfLCIS.java
@@ -10,14 +10,18 @@ public class FindLengthOfLCIS {
 
     public int findLengthOfLCIS(int[] nums) {
 
-        if(Objects.isNull(nums) || nums.length == 0) return 0;
+        if(Objects.isNull(nums) || nums.length == 0) {
+            return 0;
+        }
 
         int l = 0;
         int maxLength = 0;
 
         while(l < nums.length){
             int r = l + 1;
-            while(r < nums.length && nums[r] > nums[r - 1]) r++;
+            while(r < nums.length && nums[r] > nums[r - 1]) {
+                r++;
+            }
             maxLength = Math.max(maxLength, r - l);
 
             l = r;
